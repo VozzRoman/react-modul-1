d029aa7b193447349deecae0d1e9827d2e2a1ca3
VozzRoman <looper0500@ukr.net>
2022-12-07 20:21:13 +0200

    adding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import Painting from "./components/Painting";
-import painting from './painting.json';
+import paintings from './paintings.json';
 export default function App() {
 	return ( 
-	<div>
-		< Painting
-		url={painting[0].url}
-		title={painting[0].title}
-		price={painting[0].price}
-		author={painting[0].author.blog} />
+		<div>
+			{paintings.map(paint => (
+				< Painting // компонент
+		key={paint.id} // ключ что бы не было ошибок при рендре разметки
+		imageUrl={paint.url} // пропсы
+		title={paint.title} // пропсы
+		price={paint.price} // пропсы
+		author={paint.author.blog} // пропсы
+		quantity={paint.quantity}
+			/>
+			))}
+	
+					{/* < Painting
+		imageUrl={painting[1].url}
+		title={painting[1].title}
+		price={painting[1].price}
+		// author={painting[1].author.blog} - не пришло значение (дефлотное значение author = 'unkomwn')
+		quantity={painting[1].quantity}		
+			/>
+					< Painting
+		imageUrl={painting[2].url}
+		title={painting[2].title}
+		price={painting[2].price}
+		author={painting[2].author.blog}
+		quantity={painting[2].quantity}		
+		/> */}
+			
+
 	</div>
 	);
 }
+
+//Загрузить что то (это может быьб строка или разметка) по условию!!
+//Или логическое И или Тренарник
+// export default function App() {
+// 	const isOnline = false;
+
+// 	return (
+// 		<div>
+// 			{isOnline && 'Онлайн'}
+
+// 			{isOnline ? 'Онлайн' : 'Офлайн'}
+// 		</div>
+// 	)
+// }
+
+
+
+//Рендрим коллекцию
+
+// export default function App() {
+// 	return (
+// 		<div>
+// 			{[1, 2, 3, 4, 5].map(el => {
+// 				<div>{el}</div>
+// 			})}
+// 	</div>
+// 	)
+// }
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,14 +1,40 @@
 
+import PropTypes from 'prop-types'; // npm pacakge для проверки типов данных которые приходят с бекенда
+import defaultImage from './default.jpg'; // передаем дефолтное значение каринки
+
+console.log(defaultImage);
+
 export default function Painting(props) {
-	const {url, title, price, author} = props // destructorisation
+	const { imageUrl = defaultImage, title, price, author = 'unkomwn', quantity } = props // destructorisation
+	// если како-это значнеие не придет то ставим дефолтное значение author = 'unkomwn'
 	  console.log(props)
 	return (<div>
-		<img src={url} alt={title} width="480" />
+		<img src={imageUrl} alt={title} width="480" />
 		<h2>{title}</h2>
 		<p>Author: <a href=""></a>{author}</p>
 		<p>Price: {price}credits</p>
-		<p>Availibility: less off or is on</p>
+{/* по условию - если меньши 10 то товар заканчиывается если больше то товар есть в наличии */}
+		<p>Availibility: {quantity < 10 ? 'less off' : 'is on'}</p>
 		<button type="button">Add to chart</button>
 	</div>
 	);
 }
+//Проверяем на тип данных
+Painting.propTypes = {
+	imageUrl: PropTypes.string,
+	title: PropTypes.string,
+	price: PropTypes.number,
+	author: PropTypes.string,
+};
+//может быть
+// MyComponent.propTypes = {
+//   // You can declare that a prop is a specific JS primitive. By default, these
+//   // are all optional.
+// //   optionalArray: PropTypes.array,
+// //   optionalBigInt: PropTypes.bigint,
+// //   optionalBool: PropTypes.bool,
+// //   optionalFunc: PropTypes.func,
+// //   optionalNumber: PropTypes.number,
+// //   optionalObject: PropTypes.object,
+// //   optionalString: PropTypes.string,
+// //   optionalSymbol: PropTypes.symbol,
